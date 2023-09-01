4e8a3292c7823e6df6018778568b59839d625f1d
Artem <artemiy.bondar49@gmail.com>
2023-09-01 19:25:44 +0300

    2

--- README.md
+++ README.md
@@ -26,8 +26,8 @@ Users should be able to:
 
 ### Links
 
-- Solution URL: [Add solution URL here](https://github.com/artemiy43/challenge-email-form)
-- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+- Solution URL: [https://github.com/artemiy43/challenge-email-form]
+- Live Site URL: [https://challenge-email-form.vercel.app/]
 
 ## My process
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import picture_back_m from "./assets/images/illustration-sign-up-mobile.svg";
+import picture_back_d from "./assets/images/illustration-sign-up-desktop.svg";
 import Text from "./Text";
 import Form from "./Form";
 import Answer from "./Answer";
@@ -15,7 +16,9 @@ function App() {
     <>
       {!isSended ? (
         <main className="text-black text-3xl font-Roboto bg-white flex flex-col gap-12 w-full h-screen sm:w-[600px] sm:flex-row sm:grid sm:gap-6 sm:grid-cols-2 sm:grid-rows-2 sm:p-6 sm:rounded-3xl lg:w-[1000px] lg:pl-16 lg:h-full">
-          <div className="bg-mobile bg-cover bg-no-repeat w-full h-full sm:bg-desktop sm:order-2 sm:row-span-2 lg:w-[400px] lg:h-[593px] lg:place-self-end" />
+          <div
+            className={`bg-[url('./assets/images/illustration-sign-up-mobile.svg')] bg-cover bg-no-repeat w-full h-full sm:bg-[url('./assets/images/illustration-sign-up-desktop.svg')] sm:order-2 sm:row-span-2 lg:w-[400px] lg:h-[593px] lg:place-self-end`}
+          />
           <Text />
           <Form onSubmit={onSubmit} />
           {/* <img
